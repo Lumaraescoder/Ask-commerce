0424165cc4fe730ee1b89d3248cd63dbd0b68c41
zetiagocosta <38354230+zetiagocosta@users.noreply.github.com>
2023-04-20 12:23:03 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,1 @@
-export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div>Hello World</div>
-    </div>
-  );
-}
+
