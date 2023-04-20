b5e86efec22a1e8e1426628857414f330b75dec7
Rui Pinheiro <rui.pinheiro@askblue.pt>
2023-04-20 18:12:48 +0100

    fetching single product

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,12 +1,16 @@
 import { Meta } from "@/layouts/Meta";
 import { Main } from "@/templates/Main";
+import { Product } from "@/types/types";
 import product from "next-seo/lib/jsonld/product";
-
-export async function getStaticPaths() {
+import { GetStaticProps, GetStaticPaths } from "next";
+interface ProductProps {
+  product: Product
+}
+export const  getStaticPaths: GetStaticPaths  = async () =>  {
   const res = await fetch("https://fakestoreapi.com/products");
   const data = await res.json();
 
-  const paths = data.map((product) => {
+  const paths = data.map((product: Product) => {
     return {
       params: { id: product.id.toString() },
     };
@@ -18,7 +22,7 @@ export async function getStaticPaths() {
   };
 }
 
-export async function getStaticProps(context) {
+const getStaticProps: GetStaticProps<ProductProps>  = async ({context}) => {
   const id = context.params.id;
   const res = await fetch("https://fakestoreapi.com/products/" + id);
   const data = await res.json();
@@ -28,8 +32,28 @@ export async function getStaticProps(context) {
   };
 }
 
-const getSingleProduct = ({ product }) => (
+
+const renderRating = (rating: number) => {
+  const emptyStar = 'https://www.svgrepo.com/show/513120/star.svg';
+  const fullStar = 'https://www.svgrepo.com/show/444861/star.svg';
+  const stars = [];
+  console.log(stars)
+  for (let i = 1; i <= 5; i++ ) {
+     stars.push(
+      <img
+      key={i}
+      src={i <=  rating ?  emptyStar : fullStar}
+      alt={`start ${i}`}
+       />
+     )
+     return <div>{stars} </div>
+    }
+}
+
+const getSingleProduct = ({ product }) => {
+  return (
   <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
+      
     <section className="border">
       <div className="relative mx-auto max-w-screen-xl p-5">
         <div className="grid grid-cols-1 items-center gap-8 md:grid-cols-2">
@@ -51,39 +75,39 @@ const getSingleProduct = ({ product }) => (
                 <div className="flex items-center">
                   {product.rating.rate < 2 && (
                     <div className="flex">
-                    <img
-                      width="20px"
-                      src="https://www.svgrepo.com/show/444861/star.svg"
-                      alt=""
-                      className="mr-1"
-                    />
-                    <img
-                      width="20px"
-                      src="https://www.svgrepo.com/show/513120/star.svg"
-                      alt=""
-                      className="mr-1"
-                    />
-                    <img
-                      width="20px"
-                      src="https://www.svgrepo.com/show/513120/star.svg"
-                      alt=""
-                      className="mr-1"
-                    />
-                    <img
-                      width="20px"
-                      src="https://www.svgrepo.com/show/513120/star.svg"
-                      alt=""
-                      className="mr-1"
-                    />
-                    <img
-                      width="20px"
-                      src="https://www.svgrepo.com/show/513120/star.svg"
-                      alt=""
-                      className="mr-1"
-                    />
+                      <img
+                        width="20px"
+                        src="https://www.svgrepo.com/show/444861/star.svg"
+                        alt=""
+                        className="mr-1"
+                      />
+                      <img
+                        width="20px"
+                        src="https://www.svgrepo.com/show/513120/star.svg"
+                        alt=""
+                        className="mr-1"
+                      />
+                      <img
+                        width="20px"
+                        src="https://www.svgrepo.com/show/513120/star.svg"
+                        alt=""
+                        className="mr-1"
+                      />
+                      <img
+                        width="20px"
+                        src="https://www.svgrepo.com/show/513120/star.svg"
+                        alt=""
+                        className="mr-1"
+                      />
+                      <img
+                        width="20px"
+                        src="https://www.svgrepo.com/show/513120/star.svg"
+                        alt=""
+                        className="mr-1"
+                      />
                     </div>
                   )}
-                 {product.rating.rate > 2 && product.rating.rate < 3 && (
+                  {product.rating.rate >= 2 && product.rating.rate < 3 && (
                     <div className="flex">
                       <img
                         width="20px"
@@ -98,60 +122,26 @@ const getSingleProduct = ({ product }) => (
                         className="mr-1"
                       />
                       <img
-                      width="20px"
-                      src="https://www.svgrepo.com/show/513120/star.svg"
-                      alt=""
-                      className="mr-1"
-                    />
-                    <img
-                      width="20px"
-                      src="https://www.svgrepo.com/show/513120/star.svg"
-                      alt=""
-                      className="mr-1"
-                    />
-                    <img
-                      width="20px"
-                      src="https://www.svgrepo.com/show/513120/star.svg"
-                      alt=""
-                      className="mr-1"
-                    />
+                        width="20px"
+                        src="https://www.svgrepo.com/show/513120/star.svg"
+                        alt=""
+                        className="mr-1"
+                      />
+                      <img
+                        width="20px"
+                        src="https://www.svgrepo.com/show/513120/star.svg"
+                        alt=""
+                        className="mr-1"
+                      />
+                      <img
+                        width="20px"
+                        src="https://www.svgrepo.com/show/513120/star.svg"
+                        alt=""
+                        className="mr-1"
+                      />
                     </div>
                   )}
-                  {product.rating.rate > 3 && product.rating.rate < 4 && (
-                   <div className="flex">
-                      <img
-                        width="20px"
-                        src="https://www.svgrepo.com/show/444861/star.svg"
-                        alt=""
-                        className="mr-1"
-                      />
-                      <img
-                        width="20px"
-                        src="https://www.svgrepo.com/show/444861/star.svg"
-                        alt=""
-                        className="mr-1"
-                      />
-                      <img
-                        width="20px"
-                        src="https://www.svgrepo.com/show/444861/star.svg"
-                        alt=""
-                        className="mr-1"
-                      />
-                      <img
-                      width="20px"
-                      src="https://www.svgrepo.com/show/513120/star.svg"
-                      alt=""
-                      className="mr-1"
-                    />
-                    <img
-                      width="20px"
-                      src="https://www.svgrepo.com/show/513120/star.svg"
-                      alt=""
-                      className="mr-1"
-                    />
-                    </div>
-                  )}
-                  {product.rating.rate > 4 && product.rating.rate < 5 && (
+                  {product.rating.rate >= 3 && product.rating.rate < 4 && (
                     <div className="flex">
                       <img
                         width="20px"
@@ -173,70 +163,104 @@ const getSingleProduct = ({ product }) => (
                       />
                       <img
                         width="20px"
+                        src="https://www.svgrepo.com/show/513120/star.svg"
+                        alt=""
+                        className="mr-1"
+                      />
+                      <img
+                        width="20px"
+                        src="https://www.svgrepo.com/show/513120/star.svg"
+                        alt=""
+                        className="mr-1"
+                      />
+                    </div>
+                  )}
+                  {product.rating.rate >= 4 && product.rating.rate < 5 && (
+                    <div className="flex">
+                      <img
+                        width="20px"
                         src="https://www.svgrepo.com/show/444861/star.svg"
                         alt=""
                         className="mr-1"
                       />
                       <img
-                      width="20px"
-                      src="https://www.svgrepo.com/show/513120/star.svg"
-                      alt=""
-                      className="mr-1"
-                    />
+                        width="20px"
+                        src="https://www.svgrepo.com/show/444861/star.svg"
+                        alt=""
+                        className="mr-1"
+                      />
+                      <img
+                        width="20px"
+                        src="https://www.svgrepo.com/show/444861/star.svg"
+                        alt=""
+                        className="mr-1"
+                      />
+                      <img
+                        width="20px"
+                        src="https://www.svgrepo.com/show/444861/star.svg"
+                        alt=""
+                        className="mr-1"
+                      />
+                      <img
+                        width="20px"
+                        src="https://www.svgrepo.com/show/513120/star.svg"
+                        alt=""
+                        className="mr-1"
+                      />
                     </div>
                   )}
-                 {product.rating.rate >= 5 && (
+                  {product.rating.rate == 5 && (
                     <div className="flex">
-                    <img
-                      width="20px"
-                      src="https://www.svgrepo.com/show/444861/star.svg"
-                      alt=""
-                      className="mr-1"
-                    />
-                    <img
-                      width="20px"
-                      src="https://www.svgrepo.com/show/444861/star.svg"
-                      alt=""
-                      className="mr-1"
-                    />
-                    <img
-                      width="20px"
-                      src="https://www.svgrepo.com/show/444861/star.svg"
-                      alt=""
-                      className="mr-1"
-                    />
-                    <img
-                      width="20px"
-                      src="https://www.svgrepo.com/show/444861/star.svg"
-                      alt=""
-                      className="mr-1"
-                    />
-                    <img
-                      width="20px"
-                      src="https://www.svgrepo.com/show/444861/star.svg"
-                      alt=""
-                      className="mr-1"
-                    />
-                  </div>
+                      <img
+                        width="20px"
+                        src="https://www.svgrepo.com/show/444861/star.svg"
+                        alt=""
+                        className="mr-1"
+                      />
+                      <img
+                        width="20px"
+                        src="https://www.svgrepo.com/show/444861/star.svg"
+                        alt=""
+                        className="mr-1"
+                      />
+                      <img
+                        width="20px"
+                        src="https://www.svgrepo.com/show/444861/star.svg"
+                        alt=""
+                        className="mr-1"
+                      />
+                      <img
+                        width="20px"
+                        src="https://www.svgrepo.com/show/444861/star.svg"
+                        alt=""
+                        className="mr-1"
+                      />
+                      <img
+                        width="20px"
+                        src="https://www.svgrepo.com/show/444861/star.svg"
+                        alt=""
+                        className="mr-1"
+                      />
+                    </div>
                   )}
                   <p className="ml-2 text-sm font-medium text-gray-500 dark:text-gray-400">
                     {product.rating.rate} out of 5
                   </p>
                   <div className="flex ml-5">
-                  <img
+                    <img
                       width="20px"
                       src="https://www.svgrepo.com/show/507917/archive.svg"
                       alt=""
                       className="mr-1 filter grayscale"
                     />
-                  <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
-                    {product.rating.count} reviews
-                  </p>
+                    <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
+                      {product.rating.count} reviews
+                    </p>
                   </div>
-                  
                 </div>
               </div>
             </div>
+            {renderRating(product.rating.rate)}
             <div className="mt-4">
               <div className="prose max-w-none text-lg">
                 <p>{product.description}</p>
@@ -247,6 +271,7 @@ const getSingleProduct = ({ product }) => (
       </div>
     </section>
   </Main>
-);
+  )
+};
 
 export default getSingleProduct;
