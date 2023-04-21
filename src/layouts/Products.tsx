c547618ca38d5e3a9a7966efa1660055e21b9d7f
FilipeAVNunes <filipeavnunes@gmail.com>
2023-04-21 18:02:42 +0100

    GetAllProducts Tests
    Tests not working. Error message " You need to run with a version of node that supports ES Modules in the VM API. See https://jestjs.io/docs/ecmascript-modules"

--- src/layouts/Products.tsx
+++ src/layouts/Products.tsx
@@ -1,4 +1,3 @@
-import product from "next-seo/lib/jsonld/product";
 import Link from "next/link";
 import useSWR from "swr";
 
@@ -26,7 +25,8 @@ const Products = () => {
 
 
   return (
-    
+    <div>
+      <h1 className="flex justify-center mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-blue-600 from-sky-400">Ask-Commerce</span></h1>
   <div className="grid grid-cols-1 justify-center container gap-8 mt-8 mb-8 mx-auto xl:mt-12 xl:gap-16 md:grid-cols-2 xl:grid-cols-3">
 {products.map((product: Products) => (
         <div className="max-w-xs overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800" key={product.id}>
@@ -38,12 +38,13 @@ const Products = () => {
 
         <div className="flex items-center justify-between px-4 py-2 bg-gray-900">
           <h1 className="text-lg font-bold text-white">{product.price}€</h1>
-          <Link href={`/products/${product.id}`}>
-            <button className="px-2 py-1 text-xs font-semibold text-gray-900 uppercase transition-colors duration-300 transform bg-white rounded hover:bg-gray-200 focus:bg-gray-400 focus:outline-none">More info</button>
+          <Link className="no-underline" href={`/products/${product.id}`}>
+            <button className="px-2 py-1 text-xs font-semibold text-gray-900 uppercase transition-colors no-underline duration-300 bg-white rounded hover:bg-gray-200 focus:bg-gray-400 focus:outline-none">More info</button>
           </Link>
         </div>
       </div>
 ))}
+  </div>
   </div>
 )};
 
