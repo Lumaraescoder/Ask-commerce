e949f17df7d16f1d954477e6f7c034c5a52b7cd0
hugofbarcelos <92545069+hugofbarcelos@users.noreply.github.com>
2023-04-21 14:44:12 +0100

    footer
    added footer component, test to renderization

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -1,7 +1,10 @@
-import Link from 'next/link';
+
 import type { ReactNode } from 'react';
 
 import { AppConfig } from '@/utils/AppConfig';
+import Link from 'next/link';
+import { Footer } from '@/components/footer';
+
 
 type IMainProps = {
   meta: ReactNode;
@@ -60,16 +63,7 @@ const Main = (props: IMainProps) => (
 
       <main className="content py-5 text-xl">{props.children}</main>
 
-      <footer className="border-t border-gray-300 py-8 text-center text-sm">
-        © Copyright {new Date().getFullYear()} {AppConfig.title}. Made with{' '}
-        <a href="https://creativedesignsguru.com">CreativeDesignsGuru</a>.
-        {/*
-         * PLEASE READ THIS SECTION
-         * I'm an indie maker with limited resources and funds, I'll really appreciate if you could have a link to my website.
-         * The link doesn't need to appear on every pages, one link on one page is enough.
-         * For example, in the `About` page. Thank you for your support, it'll mean a lot to me.
-         */}
-      </footer>
+    <Footer/>
     </div>
   </div>
 );
