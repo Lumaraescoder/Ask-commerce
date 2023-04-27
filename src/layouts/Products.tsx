4680872adbd582874804fda3620586702e286aee
Rui Pinheiro <rui.pinheiro@askblue.pt>
2023-04-27 17:59:29 +0100

    Shop Cart Navbar Icon and Page

--- src/layouts/Products.tsx
+++ src/layouts/Products.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useState } from "react";
 import useSWR from "swr";
 
 interface Products {
@@ -13,7 +14,11 @@ interface Products {
 const getAllProducts = (url: string) => fetch(url).then((res) => res.json());
 
 const Products = () => {
-  const { data: products, error, isLoading } = useSWR('https://fakestoreapi.com/products', getAllProducts);
+  const {
+    data: products,
+    error,
+    isLoading,
+  } = useSWR("https://fakestoreapi.com/products", getAllProducts);
 
   if (error) {
     return <p>Error</p>;
@@ -25,21 +30,54 @@ const Products = () => {
 
   return (
     <div>
-      <h1 className="flex justify-center mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-blue-600 from-sky-400">Ask-Commerce</span></h1>
+      <h1 className="flex justify-center mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r to-blue-600 from-sky-400">
+          Ask-Commerce
+        </span>
+      </h1>
       <div className="grid grid-cols-1 justify-center container gap-8 mt-8 mb-8 mx-auto xl:mt-12 xl:gap-16 md:grid-cols-2 xl:grid-cols-3">
         {products.map((product: Products) => (
-          <div className="max-w-xs overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800" key={product.id}>
+          <div
+            className="max-w-xs overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800"
+            key={product.id}
+          >
             <div className="min-h-[6rem] px-4 py-2">
-              <h1 className="text-l font-bold text-gray-800 uppercase dark:text-white">{product.title}</h1>
+              <h1 className="text-l font-bold text-gray-800 uppercase dark:text-white">
+                {product.title}
+              </h1>
             </div>
 
-            <img className="object-cover w-full h-48 mt-2" src={product.image} alt={product.title} />
+            <img
+              className="object-cover w-full h-48 mt-2"
+              src={product.image}
+              alt={product.title}
+            />
 
             <div className="flex items-center justify-between px-4 py-2 bg-gray-900">
               <h1 className="text-lg font-bold text-white">{product.price}€</h1>
               <Link className="no-underline" href={`/products/${product.id}`}>
-                <button className="px-2 py-1 text-xs font-semibold text-gray-900 uppercase transition-colors no-underline duration-300 bg-white rounded hover:bg-gray-200 focus:bg-gray-400 focus:outline-none">More info</button>
+                <button className="px-2 py-1 text-xs font-semibold text-gray-900 uppercase transition-colors no-underline duration-300 bg-white rounded hover:bg-gray-200 focus:bg-gray-400 focus:outline-none">
+                  More info
+                </button>
               </Link>
+            </div>
+            <div className="flex flex-row justify-center items-center py-2">
+              <button
+                type="button"
+                className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-gray-900 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
+              >
+                Add to cart
+                <svg
+                  className="w-3.5 h-3.5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M5.071 1.243a.5.5 0 0 1 .858.514L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 6h1.717L5.07 1.243zM3.5 10.5a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0v-3z" />
+                </svg>
+              </button>
             </div>
           </div>
         ))}
@@ -49,6 +87,3 @@ const Products = () => {
 };
 
 export default Products;
-
-
-
