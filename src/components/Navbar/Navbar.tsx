657eb1864c7c12a9f02104376935f0e3342c6a50
lu_maraes <50714793+Lumaraescoder@users.noreply.github.com>
2023-05-24 15:20:41 +0100

    Fix

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -32,21 +32,17 @@ export const Navbar = () => {
     <div>
       <nav className="flex flex-wrap items-center dark:bg-gray-900 p-3">
         <Link href="/">
-          <a className="mr-4 inline-flex items-center p-2">
-            <span className="text-xl font-bold uppercase tracking-wide text-white">
+ 
+            <span className="text-xl font-bold uppercase tracking-wide text-white"/>
               Ask-Commerce
-            </span>
-          </a>
+       
         </Link>
         <div className="hidden w-full lg:inline-flex lg:w-auto lg:grow">
           <div className="flex w-full flex-col items-start lg:ml-auto lg:inline-flex lg:h-auto lg:w-auto lg:flex-row lg:items-center">
             <Link href="/">
-              <a
-                data-testid="Services"
-                className="w-full items-center justify-center rounded px-3 py-2 font-bold text-white hover:bg-gray-600 hover:text-white lg:inline-flex lg:w-auto"
-              >
+            
                 Login
-              </a>
+              
             </Link>
             <div className="relative inline-block">
               <button
