ca5e99f9b357bbe8439b072ea6bb2275082407a5
lu_maraes <50714793+Lumaraescoder@users.noreply.github.com>
2023-06-06 17:26:32 +0100

    up

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,39 +1,39 @@
 
-import { Meta, StoryFn } from '@storybook/react';
+// import { Meta, StoryFn } from '@storybook/react';
 
-import {Button, ButtonProps } from './Button';
+// import {Button, ButtonProps } from './Button';
 
-export default {
-  title: "button", 
-  component: Button,
-  argTypes:{
-    variant:{
-      control: {
-        type: "select"
-      },
-    },
-    onClick: {
-      action: "clicked"
-    }
-  }
-} as Meta
+// export default {
+//   title: "button", 
+//   component: Button,
+//   argTypes:{
+//     variant:{
+//       control: {
+//         type: "select"
+//       },
+//     },
+//     onClick: {
+//       action: "clicked"
+//     }
+//   }
+// } as Meta
 
-const Template: StoryFn<ButtonProps> = (args) => <Button {...args}/>;
+// const Template: StoryFn<ButtonProps> = (args) => <Button {...args}/>;
 
-export const Primary = Template.bind({})
-Primary.args = {
-  variant: 'primary',
-  children: "Primary button"
-}
+// export const Primary = Template.bind({})
+// Primary.args = {
+//   variant: 'primary',
+//   children: "Primary button"
+// }
 
-export const Secondary = Template.bind({})
-Secondary.args = {
-  variant: 'secondary',
-  children: "Secondary button"
-}
+// export const Secondary = Template.bind({})
+// Secondary.args = {
+//   variant: 'secondary',
+//   children: "Secondary button"
+// }
 
-export const Tertiary = Template.bind({})
-Tertiary.args = {
-  variant: 'tertiary',
-  children: "Tertiary button"
-}
+// export const Tertiary = Template.bind({})
+// Tertiary.args = {
+//   variant: 'tertiary',
+//   children: "Tertiary button"
+// }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,19 +1,19 @@
-import React, { ReactNode } from "react";
-import { StyledButton } from "./Button.styles";
+// import React, { ReactNode } from "react";
+// import { StyledButton } from "./Button.styles";
 
 
-export type ButtonProps = {
-  variant: "primary" | "secondary" | "tertiary";
-  onClick?: ()  => void;    
-  children: ReactNode;
-}
+// export type ButtonProps = {
+//   variant: "primary" | "secondary" | "tertiary";
+//   onClick?: ()  => void;    
+//   children: ReactNode;
+// }
 
-export const Button: React.FC<ButtonProps> = ({children, variant, onClick} ) => {
-  return (
-    <StyledButton variant={variant} onClick={onClick}>
-      {children}
-    </StyledButton>
-  );
-};
+// export const Button: React.FC<ButtonProps> = ({children, variant, onClick} ) => {
+//   return (
+//     <StyledButton variant={variant} onClick={onClick}>
+//       {children}
+//     </StyledButton>
+//   );
+// };
 
-export default Button
+// export default Button
